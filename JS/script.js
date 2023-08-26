2ddd1adcb3161423561d878ae2d03e35481cd161
najmulhouda <najmulhouda6@gmail.com>
2023-08-26 12:08:18 +0600

    third commit

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,12 @@
 let menu = document.querySelector('#menu');
-let navbar = document.querySelector('.navber');
+let navbar = document.querySelector('.navbar');
 
-menu.onClick =  () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.onclick = () =>{
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+menu.classList.remove('fa-times');
+navbar.classList.remove('active');
 }
