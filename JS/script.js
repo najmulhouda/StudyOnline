5003bfca208b46cd2b870be3a17447d5f9940e6f
najmulhouda <najmulhouda6@gmail.com>
2023-08-26 23:34:02 +0600

    third time commited

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,17 @@ window.onscroll = () =>{
 menu.classList.remove('fa-times');
 navbar.classList.remove('active');
 }
+
+let sliderContent = document.querySelector(".slider-content");
+let slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+
+function slideTo(index) {
+  sliderContent.style.transform = `translateX(-${index * 100}%)`;
+    currentIndex = index;
+}
+
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    slideTo(currentIndex);
+}, 3000);
